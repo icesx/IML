0c5cd6a43d73a6ba8da23d6f1f9744ca03f27f5c
i <icesxrun@gmail.com>
2019-03-17 09:13:19 +0800

    linear ok

--- linear/linearRegression2.py
+++ linear/linearRegression2.py
@@ -3,48 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def cost(x,y,theta=np.zeros((2,1))):
-    """Computes the cost of linear regression
-    theta = parameter for linear regression
-    x and y are the data points
-    This is done to monitor the cost of gradient descent"""
-    m = len(x)
-    J = 1 / (2 * m) * sum((x.dot(theta).flatten() - y) ** 2)
-    return J
+class Linear():
+    def __init__(self):
+        pass
 
+    def cost(self,x,y,theta=np.zeros((2,1))):
+        """Computes the cost of linear regression
+        theta = parameter for linear regression
+        x and y are the data points
+        This is done to monitor the cost of gradient descent"""
+        m = len(x)
+        J = 1 / (2 * m) * sum((x.dot(theta).flatten() - y) ** 2)
+        return J
 
-def gradientDesc(all_x,all_y,theta=np.zeros((1,1)),alpha=.01,iterations=10):
-    """"Gradient Descent implementation of
-    linear regression with one variable"""
-    print theta
-    J = []
-    for numbers in range(iterations):
-        m = all_y.size
-        for i in range(m):
-            one_x = np.array([all_x[i]])
-            one_y = np.array([all_y[i]])
+    def gradient_desc(self,all_x,all_y,theta=np.zeros((1,1)),alpha=.01,iterations=10):
+        J = []
+        for numbers in range(iterations):
+            m = all_y.size
+            for i in range(m):
+                one_x = np.array([all_x[i]])
+                one_y = np.array([all_y[i]])
 
-            theta[0] = theta[0] - alpha * (1 / m) * sum((one_x.dot(theta).flatten() - one_y) * one_x)
-            J.append(cost(one_x,one_y,theta))
-        print 'Cost: ' + str(J)
-        print 'theta:',theta
-        scatterPlot(all_x,all_y,yp=None,theta=theta,savePng=True,numbers=numbers)
-    return theta
+                theta[0] = theta[0] - alpha * (1 / m) * sum((one_x.dot(theta).flatten() - one_y) * one_x)
+                J.append(self.cost(one_x,one_y,theta))
+            print 'Cost: ' + str(J)
+            print 'theta:',theta
+            self.scatter_plot(self,all_x,all_y,yp=None,theta=theta,savePng=True,numbers=numbers)
+        return theta
 
-
-# scatterplot of data with option to save figure.
-def scatterPlot(x,y,yp=None,theta=0.1,savePng=False,numbers=0):
-    plt.xlabel('Population of City in 10,000s')
-    plt.ylabel('Profit in $10,000s')
-    plt.scatter(x,y,marker='x')
-    line_x = np.arange(0,25)
-    line_y = theta[0] * line_x
-    plt.plot(line_x,line_y)
-    if yp != None:
-        plt.plot(x,yp)
-    if savePng == False:
-        plt.show()
-    else:
+    # scatterplot of data with option to save figure.
+    def scatter_plot(self,x,y,theta=0.1,numbers=0):
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.scatter(x,y,marker='x')
+        line_x = np.arange(0,25)
+        line_y = theta[0] * line_x
+        plt.plot(line_x,line_y)
         plt.savefig(str(numbers) + '.png')
 
 
@@ -53,4 +47,4 @@ if __name__ == '__main__':
     data = np.genfromtxt('ex1data2.txt',delimiter=',')
     all_x = data[:,0]
     all_y = data[:,1]
-    theta=gradientDesc(all_x,all_y)
+    theta = Linear().gradient_desc(all_x,all_y)

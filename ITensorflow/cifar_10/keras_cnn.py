5d8c819b647b9b50c44b8ddf39903b55dca11cdb
chengtao <chengtao@ithink>
2020-05-03 18:40:37 +0800

    keras_cnn

--- ITensorflow/cifar_10/keras_cnn.py
+++ ITensorflow/cifar_10/keras_cnn.py
@@ -41,6 +41,8 @@ def create_module():
     train_images, test_images, train_labels, test_labels = keras_load_data(keras.datasets.cifar10)
     verify(train_images,train_labels)
     model = models.Sequential()
+    #Conv2D(32,(3,3)) 32,卷积核的数目（即输出的维度）;(3,3)kernel size,卷积核尺寸
+    #input_shape,32*32*3,x=32,y=32,color=3
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))

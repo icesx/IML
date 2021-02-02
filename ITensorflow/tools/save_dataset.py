94426bf91fc5f366e17b989b6c5b75a011699bdc
chengtao <icesxrun@gmail.com>
2021-02-02 15:28:49 +0800

    add mnit dataset save to local

--- ITensorflow/tools/save_dataset.py
+++ ITensorflow/tools/save_dataset.py
@@ -10,21 +10,27 @@ from tensorflow import keras
 
 from tools.load_data import keras_load_data
 
-class_names = ['T-shirt_top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle_boot']
 
-
-def save_fashion_mnist(workdir):
+def save_fashion_mnist(workdir="/WORK/datasset/fashion_mnist"):
+    class_names = ['T-shirt_top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle_boot']
     train_images, test_images, train_labels, test_labels = keras_load_data(keras.datasets.fashion_mnist)
-    print(train_labels)
-    index = 0
-    for image, lable in zip(train_images, train_labels):
+    __save_dateset(class_names, train_images, train_labels, workdir)
+
+
+def __save_dateset(class_names, train_images, train_labels, workdir):
+    for index, (image, lable) in enumerate(zip(train_images, train_labels)):
         workdir_folder = workdir + "/" + class_names[lable]
         if os.path.exists(workdir_folder) is False:
             os.mkdir(workdir_folder)
         plt.imsave(workdir_folder + "/" + str(index) + ".jpg", image)
-        index += 1
+
+
+def save_mnist(workdir="/WORK/datasset/mnist"):
+    train_images, test_images, train_labels, test_labels = keras_load_data(keras.datasets.mnist)
+    class_names = ['0', '1', "2", "3", "4", "5", "6", "7", "8", "9"]
+    __save_dateset(class_names, train_images, train_labels, workdir)
 
 
 if __name__ == '__main__':
-    save_fashion_mnist("/WORK/datasset/fashion_mnist")
+    save_mnist()

d52c3384ff5ef948efdc26e2243816d67188c98e
chengtao <icesxrun@gmail.com>
2021-02-04 09:48:04 +0800

    add cifa10

--- ITensorflow/tools/save_module.py
+++ ITensorflow/tools/save_module.py
@@ -5,12 +5,13 @@
 import tensorflow as tf
 from tensorflow import keras
 
-def save_module(module,export_dir):
-    print("save module ",module,"to dir ",export_dir,"..")
-    tf.saved_model.save(module,export_dir=export_dir)
+
+def save_module(module, export_dir):
+    print("save module ", module, "to dir ", export_dir, "..")
+    tf.saved_model.save(module, export_dir=export_dir)
     print("saved")
 
 
 def load_module(export_dir):
-    print("load module from ",export_dir)
+    print("load module from ", export_dir)
     return keras.models.load_model(export_dir)
